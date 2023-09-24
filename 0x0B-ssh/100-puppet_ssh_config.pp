c4dc12137c0b0a1155ebe876a14eaf2bcf0d94e1
FatimaZahraOukmi <Fatimazahra.oukmi@gmail.com>
2023-09-24 05:51:20 -0700

    auto

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,14 +2,14 @@
 
 include stdlib
 
-file_line { 'Refuse to authenticate using a password':
+file_line {'Refuse to authenticate using a password':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
 
-file_line { 'Use private key':
+file_line {'Use private key':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school'
+  line   => 'IdentityFile ~/.ssh/school',
 }
